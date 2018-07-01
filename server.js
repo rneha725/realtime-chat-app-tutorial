a946456818f8220dcd0c8c4dc07dfa9a5cacee70
Neha Rani <i344768@inll50910487a.apj.global.corp.sap>
2018-07-02 00:58:58 +0530

    Commit with API errors

---
+++ server.js
@@ -0,0 +1,60 @@
+const cors = require('cors');
+const next = require('next');
+const Pusher = require('pusher');
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+const Sentiment = require('sentiment');
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+
+const app = next({dev});
+const handler = app.getRequestHandler();
+const sentiment = new Sentiment();
+
+const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_APP_KEY,
+    secret: process.env.PUSHER_APP_SECRET,
+    cluster: process.env.PUSHER_APP_CLUSTER,
+    encrypted: true
+});
+
+app.prepare()
+    .then(()=>
+    {
+        const server = express();
+
+        server.use(cors());
+        server.use(bodyParser.json());
+        server.use(bodyParser.urlencoded({ extended: true }));
+
+        server.get('*', function(req, res) {
+            return handler(req, res);
+        });
+
+        const chatHistory = { messages: [] };
+
+        server.post('/message', (req, res, err) => {
+            const { user = null, message = '', timestamp = +new Date } = req.body;
+            const sentimentScore = sentiment.analyze(message).score;
+            const chat = { user, message, timestamp, sentiment: sentimentScore };
+            chatHistory.messages.push(chat);
+
+            pusher.trigger('chat-room', 'new-message', { chat });
+        })
+
+        server.post('/messages', (req, res, err) => {
+            res.json({...chatHistory, status: 'success'});
+        })
+
+        server.listen(port, err => {
+            if(err) throw err;
+            console.log(`port.. ${port}`);
+        });
+    })
+    .catch(ex => {
+        console.error(ex.stack);
+        process.exit(1);
+    })
