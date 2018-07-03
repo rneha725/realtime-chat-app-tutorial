eedf770c964bfb7ca71d5e7ab04eaffc7aa8f6f6
Neha Rani <i344768@inll50910487a.apj.global.corp.sap>
2018-07-03 15:44:47 +0530

    logged(not really) user can be changed without refresh

--- server.js
+++ server.js
@@ -9,52 +9,51 @@ const Sentiment = require('sentiment');
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
 
-const app = next({dev});
+const app = next({ dev });
 const handler = app.getRequestHandler();
 const sentiment = new Sentiment();
 
 const pusher = new Pusher({
-    appId: process.env.PUSHER_APP_ID,
-    key: process.env.PUSHER_APP_KEY,
-    secret: process.env.PUSHER_APP_SECRET,
-    cluster: process.env.PUSHER_APP_CLUSTER,
-    encrypted: true
+	appId: process.env.PUSHER_APP_ID,
+	key: process.env.PUSHER_APP_KEY,
+	secret: process.env.PUSHER_APP_SECRET,
+	cluster: process.env.PUSHER_APP_CLUSTER,
+	encrypted: true
 });
 
 app.prepare()
-    .then(()=>
-    {
-        const server = express();
+	.then(() => {
+		const server = express();
 
-        server.use(cors());
-        server.use(bodyParser.json());
-        server.use(bodyParser.urlencoded({ extended: true }));
+		server.use(cors());
+		server.use(bodyParser.json());
+		server.use(bodyParser.urlencoded({ extended: true }));
 
-        server.get('*', function(req, res) {
-            return handler(req, res);
-        });
+		server.get('*', function (req, res) {
+			return handler(req, res);
+		});
 
-        const chatHistory = { messages: [] };
+		const chatHistory = { messages: [] };
 
-        server.post('/message', (req, res, err) => {
-            const { user = null, message = '', timestamp = +new Date } = req.body;
-            const sentimentScore = sentiment.analyze(message).score;
-            const chat = { user, message, timestamp, sentiment: sentimentScore };
-            chatHistory.messages.push(chat);
+		server.post('/message', (req, res, err) => {
+			const { user = null, message = '', timestamp = +new Date } = req.body;
+			const sentimentScore = sentiment.analyze(message).score;
+			const chat = { user, message, timestamp, sentiment: sentimentScore };
+			chatHistory.messages.push(chat);
 
-            pusher.trigger('chat-room', 'new-message', { chat });
-        })
+			pusher.trigger('chat-room', 'new-message', { chat });
+		})
 
-        server.post('/messages', (req, res, err) => {
-            res.json({...chatHistory, status: 'success'});
-        })
+		server.post('/messages', (req, res, err) => {
+			res.json({ ...chatHistory, status: 'success' });
+		})
 
-        server.listen(port, err => {
-            if(err) throw err;
-            console.log(`port.. ${port}`);
-        });
-    })
-    .catch(ex => {
-        console.error(ex.stack);
-        process.exit(1);
-    })
+		server.listen(port, err => {
+			if (err) throw err;
+			console.log(`port.. ${port}`);
+		});
+	})
+	.catch(ex => {
+		console.error(ex.stack);
+		process.exit(1);
+	})
